79fbd73e8f4c344836506d5ed18f4df9b6ef9190
jbcarras <jbcarras@buffalo.edu>
2026-03-11 12:22:55 -0400

    Fix active visits route and some frontend reorganization

---
+++ client/src/components/TableEntry.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const props = defineProps(['data'])
+
+</script>
+
+<template>
+          <tr>
+            <td v-for="entry in data">{{ entry }}</td>
+            <slot/>
+          </tr>
+</template>
+
+<style scoped>
+
+
+  tr, th, td {
+    border: 2px solid #D9D9D9;
+    border-collapse: collapse;
+  }
+
+  td {
+    padding: 8px;
+  }
+
+  tr:nth-child(odd) {
+    background-color: #FAFAFA;
+  }
+
+
+</style>
